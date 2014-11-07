6b1933ff481873fd3a3812dbbabc878391d3879c
Pure Krome <github@world-domination.com.au>
2014-11-07 11:11:37 +1100

    Pew Pew!

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shouldly;
+using Xunit;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        [Fact]
+        public void GivenAFunkyString_EscapeDataString_EncodesTheData()
+        {
+            // Arrange.
+            const string data = "abcde *.(.).";
+
+            // Act.
+            var result = Uri.EscapeDataString(data);
+
+            // Assert.
+            result.ShouldBe("abcde%20%2A.%28.%29.");
+        }
+    }
+}
---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApplication2
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            const string data = "abcde *.(.).";
+
+            var result = Uri.EscapeDataString(data);
+
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}
